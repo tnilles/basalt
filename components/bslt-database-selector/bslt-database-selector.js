c04221f26171f75677e3ffbfb94723681d9a5837
Thierry Nilles <contact@thierrynilles.com>
2015-04-29 20:04:01 -0400

    Color the current database

--- components/bslt-database-selector/bslt-database-selector.js
+++ components/bslt-database-selector/bslt-database-selector.js
@@ -3,6 +3,14 @@
 Polymer({
     databases: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
     selectDB: function (event, detail, sender) {
+        var dbs = sender.parentElement.querySelectorAll('li');
+
+        [].forEach.call(dbs, function(element) {
+            element.classList.remove('selected');
+        });
+
+        sender.classList.add('selected');
+
         this.fire('select-database', sender.getAttribute('data-database'));
     }
 });
