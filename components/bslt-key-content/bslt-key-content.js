9983bd1ca33fd7a59b5ae4ddef6bcd0574a16d4b
Thierry Nilles <contact@thierrynilles.com>
2015-05-06 23:35:35 -0400

    Clean up

--- components/bslt-key-content/bslt-key-content.js
+++ components/bslt-key-content/bslt-key-content.js
@@ -3,15 +3,6 @@
 Polymer({
     content: {},
     type: '',
-    ready: function () {
-        console.log('content!')
-    },
-    contentChanged: function () {
-        console.log(this.content, ' changed!')
-    },
-    typeChanged: function () {
-        console.log(this.type, ' changed...')
-    },
     getKeys: function (o) {
         return Object.keys(o);
     }
